ffd622bac2938bcaef417a92f55873b197bae194
Noel <noel@thes-mbp.apollosrc.root>
2016-11-21 16:24:09 -0600

    Inventory

---
+++ src/com/company/ListItem.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by noel on 11/21/16.
+ */
+public class ListItem {
+    public String text;
+
+    public ListItem(String text, int number) {
+        this.text = text;
+        this.number = number;
+    }
+
+    public int number;
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<ListItem> items = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int i = 1;
+            for (ListItem item : items) {
+                String checkbox = "[" + item.number + "]";
+
+
+                System.out.println( i + ". " + item.text  + checkbox );
+                i++; }
+
+            System.out.println("1. Enter item name");
+            System.out.println("2. Remove an item");
+            System.out.println("3. Edit item quantity");
+
+            String option = scanner.nextLine();
+
+            if (option.equals("1")) {
+                System.out.println("Enter item");
+                String text = scanner.nextLine();
+
+                ListItem item = new ListItem(text, 0);
+                items.add(item);
+
+
+
+            } else if (option.equals("2")) {
+                System.out.println("Enter the number of the item you want to remove:");
+                int itemNum = Integer.parseInt(scanner.nextLine());
+
+                items.remove(itemNum - 1);
+
+            } else if(option.equals("3")) {
+
+                System.out.println("Choose the item you want to edit");
+                int itemNum = Integer.parseInt(scanner.nextLine());
+                System.out.println("What would you like to edit quantiy to?...");
+                int number = Integer.parseInt(scanner.nextLine());
+
+                ListItem item = items.get(itemNum - 1);
+                item.number = number;
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
